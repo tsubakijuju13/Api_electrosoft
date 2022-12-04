f1a6bafa32b9711669611ad7ee93cc21d3e21efc
tsubakijuju13 <santiago.ramirez.ospina@correounivalle.edu.co>
2022-12-04 03:30:59 +0000

    testing auth/registration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +127,24 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication' 
+    ],
+}
+
+CORS_ALLOWED_ORIGINS = [
+    "https://8080-cs-966470023409-default.cs-us-east1-vpcf.cloudshell.dev",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "https://8080-cs-966470023409-default.cs-us-east1-vpcf.cloudshell.dev",
+    ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
+SITE_ID = 1
