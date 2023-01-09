133293d1b98531cdf7edf2fb5c4ef7842bb27522
KPOETA1 <geider.montano@correounivalle.edu.co>
2023-01-09 14:10:44 -0500

    unpoquito de "ayuda"
    se eliminara el apartado de contacto

--- frontend/src/components/Empresa/Ayuda.jsx
+++ frontend/src/components/Empresa/Ayuda.jsx
@@ -1,40 +1,92 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import "./ayuda.css";
 
 const Ayuda = () => {
-
   const [selected, setSelected] = useState(null);
 
   const toggle = (index) => {
     if (selected === index) {
-      return setSelected(null)
+      return setSelected(null);
     }
 
     setSelected(index);
   };
 
+  const [formStatus, setFormStatus] = React.useState("Send");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setFormStatus("Submitting...");
+    const { name, email, message } = e.target.elements;
+    let conFom = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    console.log(conFom);
+  };
+
   return (
+    <div className="background">
     <div className="wrapper">
       <div className="accordion">
         {data.map((item, index) => (
           <div className="item">
-            <div className="title" onClick={() => toggle(index)}>
+            <div className="title50" onClick={() => toggle(index)}>
               <h2>{item.pregunta}</h2>
-                <span>{selected === index ? "-" : "+"}</span>
+              <span>{selected === index ? "-" : "+"}</span>
             </div>
-            <div className={selected === index ? "content show" : "content"}> {item.respuesta} </div>
+            <div className={selected === index ? "content show" : "content"}>
+              {" "}
+              {item.respuesta}{" "}
+            </div>
           </div>
         ))}
       </div>
+      
+        <div className="contactanos">
+          <h2 className="mb-3 mt-3">Contactanos</h2>
+          <form onSubmit={onSubmit}>
+            <div className="mb-3">
+              <label className="form-label" htmlFor="name">
+                Nombre
+              </label>
+              <input className="ayuda-entrada" type="text" id="name" required />
+            </div>
+            <div className="mb-3">
+              <label className="form-label" htmlFor="email">
+                Correo
+              </label>
+              <input
+                className="ayuda-entrada"
+                type="email"
+                id="email"
+                required
+
+              />
+            </div >
+            <div className="mb-3 " >
+              <label className="form-label" htmlFor="message">
+                Mensaje
+              </label>
+              <textarea className="caja" id="message" required />
+            </div>
+            <button className="btn btn-danger" type="submit">
+              {formStatus}
+            </button>
+          </form>
+        </div>
+     
+    </div>
     </div>
   );
-}
+};
 
 const data = [
   {
     pregunta: "¿Qué es la plataforma?",
-    respuesta: "Es una plataforma que permite a los usuarios encontrar y contratar servicios de manera fácil y segura.",
+    respuesta:
+      "Es una plataforma que permite a los usuarios encontrar y contratar servicios de manera fácil y segura.",
   },
   {
     pregunta: "¿Cómo funciona?",
@@ -48,19 +100,23 @@ const data = [
   },
   {
     pregunta: "¿Cómo puedo iniciar sesión?",
-    respuesta:"Para iniciar sesión, debe ingresar a la plataforma y completar el formulario de inicio de sesión.",
+    respuesta:
+      "Para iniciar sesión, debe ingresar a la plataforma y completar el formulario de inicio de sesión.",
   },
   {
     pregunta: "¿Cómo puedo publicar un servicio?",
-    respuesta:"Para publicar un servicio, debe ingresar a la plataforma y completar el formulario de publicación de servicios.",
+    respuesta:
+      "Para publicar un servicio, debe ingresar a la plataforma y completar el formulario de publicación de servicios.",
   },
   {
     pregunta: "¿Cómo puedo buscar un servicio?",
-    respuesta:"Para buscar un servicio, debe ingresar a la plataforma y completar el formulario de búsqueda de servicios.",
+    respuesta:
+      "Para buscar un servicio, debe ingresar a la plataforma y completar el formulario de búsqueda de servicios.",
   },
   {
     pregunta: "¿Cómo puedo contactar a un proveedor?",
-    respuesta:"Para contactar a un proveedor, debe ingresar a la plataforma y completar el formulario de contacto.",
+    respuesta:
+      "Para contactar a un proveedor, debe ingresar a la plataforma y completar el formulario de contacto.",
   },
 ];
 
