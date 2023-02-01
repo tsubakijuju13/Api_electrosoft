4977ca5910c58c7d88ad081ef48f764678651a08
つばき_0x01 <santiago.ramirez.ospina@correounivalle.edu.co>
2023-02-01 13:32:22 -0500

    Correo

--- backend/settings.py
+++ backend/settings.py
@@ -221,4 +221,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587 
 EMAIL_HOST_USER = 'electuv@gmail.com'
-EMAIL_HOST_PASSWORD = 'kyhqlhamrqmaoyyw'
+EMAIL_HOST_PASSWORD = 'qrzwoelutctvnvkl'
